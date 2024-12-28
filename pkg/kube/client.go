8de8c4ef7deb28a9c7cc84584ecc755cb1255f02
George Jenkins <gvjenkins@gmail.com>
2024-12-28 10:52:26 -0800

    retries
    Signed-off-by: George Jenkins <gvjenkins@gmail.com>

--- pkg/kube/client.go
+++ pkg/kube/client.go
@@ -610,110 +610,114 @@ func isIncompatibleServerError(err error) bool {
 }
 
 func patchResource(info *resource.Info) error {
-	helper := resource.NewHelper(
-		info.Client,
-		info.Mapping).
-		DryRun(false).
-		WithFieldManager(fieldManagerServerSideApply).
-		WithFieldValidation("Strict")
+	return retry.RetryOnConflict(
+		retry.DefaultRetry,
+		func() error {
+			helper := resource.NewHelper(
+				info.Client,
+				info.Mapping).
+				DryRun(false).
+				WithFieldManager(fieldManagerServerSideApply).
+				WithFieldValidation("Strict")
 
-	// Send the full object to be applied on the server side.
-	data, err := runtime.Encode(unstructured.UnstructuredJSONScheme, info.Object)
-	if err != nil {
-		return cmdutil.AddSourceToErr("serverside-apply", info.Source, err)
-	}
-	ForceConflicts := false
-	options := metav1.PatchOptions{
-		Force: &ForceConflicts,
-	}
-	obj, err := helper.Patch(
-		info.Namespace,
-		info.Name,
-		types.ApplyPatchType,
-		data,
-		&options,
-	)
-	if err != nil {
-		if isIncompatibleServerError(err) {
-			err = fmt.Errorf("Server-side apply not available on the server: (%v)", err)
-		}
-		if apierrors.IsConflict(err) {
-			err = fmt.Errorf(`%v
-Please review the fields above--they currently have other managers. Here
-are the ways you can resolve this warning:
-* If you intend to manage all of these fields, please re-run the apply
-  command with the `+"`--force-conflicts`"+` flag.
-* If you do not intend to manage all of the fields, please edit your
-  manifest to remove references to the fields that should keep their
-  current managers.
-* You may co-own fields by updating your manifest to match the existing
-  value; in this case, you'll become the manager if the other manager(s)
-  stop managing the field (remove it from their configuration).
-See https://kubernetes.io/docs/reference/using-api/server-side-apply/#conflicts`, err)
-		}
-		return err
-	}
+			// Send the full object to be applied on the server side.
+			data, err := runtime.Encode(unstructured.UnstructuredJSONScheme, info.Object)
+			if err != nil {
+				return cmdutil.AddSourceToErr("serverside-apply", info.Source, err)
+			}
+			ForceConflicts := false
+			options := metav1.PatchOptions{
+				Force: &ForceConflicts,
+			}
+			obj, err := helper.Patch(
+				info.Namespace,
+				info.Name,
+				types.ApplyPatchType,
+				data,
+				&options,
+			)
+			if err != nil {
+				if isIncompatibleServerError(err) {
+					err = fmt.Errorf("Server-side apply not available on the server: (%v)", err)
+				}
+				if apierrors.IsConflict(err) {
+					err = fmt.Errorf(`%v
+	Please review the fields above--they currently have other managers. Here
+	are the ways you can resolve this warning:
+	* If you intend to manage all of these fields, please re-run the apply
+	command with the `+"`--force-conflicts`"+` flag.
+	* If you do not intend to manage all of the fields, please edit your
+	manifest to remove references to the fields that should keep their
+	current managers.
+	* You may co-own fields by updating your manifest to match the existing
+	value; in this case, you'll become the manager if the other manager(s)
+	stop managing the field (remove it from their configuration).
+	See https://kubernetes.io/docs/reference/using-api/server-side-apply/#conflicts`, err)
+				}
+				return err
+			}
 
-	//info.Refresh(obj, true)
+			//info.Refresh(obj, true)
 
-	// Migrate managed fields if necessary.
-	//
-	// By checking afterward instead of fetching the object beforehand and
-	// unconditionally fetching we can make 3 network requests in the rare
-	// case of migration and 1 request if migration is unnecessary.
-	//
-	// To check beforehand means 2 requests for most operations, and 3
-	// requests in worst case.
-	//if err = o.saveLastApplyAnnotationIfNecessary(helper, info); err != nil {
-	//	fmt.Fprintf(o.ErrOut, warningMigrationLastAppliedFailed, err.Error())
-	//} else if performedMigration, err := o.migrateToSSAIfNecessary(helper, info); err != nil {
-	//	// Print-error as a warning.
-	//	// This is a non-fatal error because object was successfully applied
-	//	// above, but it might have issues since migration failed.
-	//	//
-	//	// This migration will be re-attempted if necessary upon next
-	//	// apply.
-	//	fmt.Fprintf(o.ErrOut, warningMigrationPatchFailed, err.Error())
-	//} else if performedMigration {
-	//	if obj, err = helper.Patch(
-	//		info.Namespace,
-	//		info.Name,
-	//		types.ApplyPatchType,
-	//		data,
-	//		&options,
-	//	); err != nil {
-	//		// Re-send original SSA patch (this will allow dropped fields to
-	//		// finally be removed)
-	//		fmt.Fprintf(o.ErrOut, warningMigrationReapplyFailed, err.Error())
-	//	} else {
-	//		info.Refresh(obj, false)
-	//	}
-	//}
+			// Migrate managed fields if necessary.
+			//
+			// By checking afterward instead of fetching the object beforehand and
+			// unconditionally fetching we can make 3 network requests in the rare
+			// case of migration and 1 request if migration is unnecessary.
+			//
+			// To check beforehand means 2 requests for most operations, and 3
+			// requests in worst case.
+			//if err = o.saveLastApplyAnnotationIfNecessary(helper, info); err != nil {
+			//	fmt.Fprintf(o.ErrOut, warningMigrationLastAppliedFailed, err.Error())
+			//} else if performedMigration, err := o.migrateToSSAIfNecessary(helper, info); err != nil {
+			//	// Print-error as a warning.
+			//	// This is a non-fatal error because object was successfully applied
+			//	// above, but it might have issues since migration failed.
+			//	//
+			//	// This migration will be re-attempted if necessary upon next
+			//	// apply.
+			//	fmt.Fprintf(o.ErrOut, warningMigrationPatchFailed, err.Error())
+			//} else if performedMigration {
+			//	if obj, err = helper.Patch(
+			//		info.Namespace,
+			//		info.Name,
+			//		types.ApplyPatchType,
+			//		data,
+			//		&options,
+			//	); err != nil {
+			//		// Re-send original SSA patch (this will allow dropped fields to
+			//		// finally be removed)
+			//		fmt.Fprintf(o.ErrOut, warningMigrationReapplyFailed, err.Error())
+			//	} else {
+			//		info.Refresh(obj, false)
+			//	}
+			//}
 
-	//WarnIfDeleting(info.Object, o.ErrOut)
+			//WarnIfDeleting(info.Object, o.ErrOut)
 
-	//if err := o.MarkObjectVisited(info); err != nil {
-	//	return err
-	//}
+			//if err := o.MarkObjectVisited(info); err != nil {
+			//	return err
+			//}
 
-	//if o.shouldPrintObject() {
-	//	return nil
-	//}
+			//if o.shouldPrintObject() {
+			//	return nil
+			//}
 
-	//printer, err := o.ToPrinter("serverside-applied")
-	//if err != nil {
-	//	return err
-	//}
+			//printer, err := o.ToPrinter("serverside-applied")
+			//if err != nil {
+			//	return err
+			//}
 
-	//if err = printer.PrintObj(info.Object, o.Out); err != nil {
-	//	return err
-	//}
+			//if err = printer.PrintObj(info.Object, o.Out); err != nil {
+			//	return err
+			//}
 
-	//obj, err := resource.NewHelper(info.Client, info.Mapping).WithFieldManager(getManagedFieldsManager()).Create(info.Namespace, true, info.Object)
-	//if err != nil {
-	//	return err
-	//}
-	return info.Refresh(obj, true)
+			//obj, err := resource.NewHelper(info.Client, info.Mapping).WithFieldManager(getManagedFieldsManager()).Create(info.Namespace, true, info.Object)
+			//if err != nil {
+			//	return err
+			//}
+			return info.Refresh(obj, true)
+		})
 }
 
 func deleteResource(info *resource.Info, policy metav1.DeletionPropagation) error {
